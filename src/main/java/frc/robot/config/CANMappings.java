9adebfa6d1c3e391f0f1e60feb9e4ff2d03674fa
graysodastudios <graysodastudios@gmail.com>
2026-01-20 16:40:07 -0500

    Hopper and Spindexer Subsystems

--- src/main/java/frc/robot/config/CANMappings.java
+++ src/main/java/frc/robot/config/CANMappings.java
@@ -1,3 +1,8 @@
 package frc.robot.config;
 
-public class CANMappings {}
+public class CANMappings {
+  public static final int SPINDEXER_MOTOR_ID = 0;
+
+  public static final int LEFT_HOPPER_MOTOR_ID = 0;
+  public static final int RIGHT_HOPPER_MOTOR_ID = 0;
+}
---
+++ src/main/java/frc/robot/config/SpindexerConfig.java
@@ -0,0 +1,7 @@
+package frc.robot.config;
+
+public class SpindexerConfig {
+  public static final double SPINDEXER_SUPPLY_CURRENT_LIMIT = 70;
+  public static final double SPINDEXER_STATOR_CURRENT_LIMIT = 80;
+  public static final double SPINDEXER_GEAR_RATIO = 0;
+}
